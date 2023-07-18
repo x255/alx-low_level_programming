716d91755d4f8d34bedb8db199917274da5e1668
Chris Munyau <munyau.chris@yahoo.com>
2023-07-18 16:17:48 -0700

    second commits

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -34,11 +34,13 @@ _putchar((k / 10) + '0');
 _putchar((k % 10) + '0');
 }
 else if (k >= 100)
+{
 _putchar(',');
 _putchar(' ');
 _putchar((k / 100) + '0');
 _putchar(((k / 10) % 10) + '0');
 _putchar((k % 10) + '0');
+}
 }
 _putchar('\n');
 }
