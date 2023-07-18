53b1043d16f8ed1214d078a375c2a2aa875dced3
Chris Munyau <munyau.chris@yahoo.com>
2023-07-18 15:13:00 -0700

    Today we are doing functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * main - prints _putchar, fllowd by a new linees
- * Return: Alwys 0 (Success)
- */
+* main - prints _putchar, fllowd by a new linees
+* Return: Alwys 0 (Success)
+*/
 int main(void)
 {
   char abe[] = "_putchar";
