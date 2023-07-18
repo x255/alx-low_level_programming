4a155ec7b133890e5f023981afd591ed29421c34
Chris Munyau <munyau.chris@yahoo.com>
2023-07-18 15:29:46 -0700

    Today we are doing functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * main - prints _putchar, followed by a new line
  *
@@ -16,6 +15,5 @@ int main(void)
   _putchar('a');
   _putchar('r');
   _putchar('\n');
-
   return (0);
 }
